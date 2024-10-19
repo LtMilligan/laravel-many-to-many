cd15c5a63fffda4a3f422d114cdeae299793feca
LtMilligan <gioele.miscia@gmail.com>
2024-10-20 01:25:07 +0200

    [UPD] Add project table and seeder

--- database/seeders/TechnologySeeder.php
+++ database/seeders/TechnologySeeder.php
@@ -2,6 +2,7 @@
 namespace Database\Seeders;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use App\Models\Technology;
 class TechnologySeeder extends Seeder
 {
     /**
@@ -11,6 +12,13 @@ class TechnologySeeder extends Seeder
      */
     public function run()
     {
-        //
+        $technologies = ['HTML', 'CSS', 'Javascript', 'Vuejs', 'PHP', 'MySQL', 'Laravel', 'Symfony', 'NodeJS', 'UI/UX design'];
+        foreach ($technologies as $technology) {
+            $new_technology = new Technology();
+            $new_technology->name = $technology;
+            $new_technology->slug = Technology::generateSlug($technology);
+            
+            $new_technology->save();
+        }
     }
 }
